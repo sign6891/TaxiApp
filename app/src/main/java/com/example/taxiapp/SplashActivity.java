29171042cc540dfb35f487fa04f96aa2fd4202cd
Janessa Leyshren <jleyshre@at-c3.21-school.ru>
2020-02-27 10:03:50 +0300

    version 1

---
+++ app/src/main/java/com/example/taxiapp/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.taxiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashActivity.this, ChooseModeActivity.class));
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
